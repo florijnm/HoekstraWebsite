d1570a92e9f0120f644def403749eb43cd5aa33c
Florijnm <florijn.munster@student.nhlstenden.com>
2021-03-26 14:20:29 +0100

    Database connectie op index pagina

---
+++ Hoekstra Fotografie/Hoekstra Fotografie/Website Hoekstra/Website Hoekstra/DBRepos.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace Website_Hoekstra
+{
+    public class DBRepos
+    {
+        public IDbConnection Connect()
+        {
+            string connectionString = @"Server=127.0.0.1;Port=3306;Database=hoekstrafotografie;Uid=root;Pwd='';";
+            return new MySqlConnection(connectionString);
+        }
+        public List<user_controller> Get()
+        {
+            var connect = Connect();
+            List<user_controller> users = connect.Query<user_controller>(sql: "SELECT * FROM users").ToList();
+            return users;
+        }
+    }
+}
---
+++ Hoekstra Fotografie/Hoekstra Fotografie/Website Hoekstra/Website Hoekstra/user_controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website_Hoekstra
+{
+    public class user_controller
+    {
+        public string first_name;
+        public string last_name;
+        public string email;
+        public string password;
+        public string username;
+        public int user_id;
+        public bool admin;
+    }
+}
