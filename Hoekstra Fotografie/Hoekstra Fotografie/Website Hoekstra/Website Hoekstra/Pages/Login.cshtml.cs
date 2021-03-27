874adeb19c78e87b2ce65a971e3e86eb2fd5532a
Nick Riemersma <honossillie@gmail.com>
2021-03-27 21:38:25 +0100

    placeholder navbar registreren / hashen en salten werkt login pagina nog placeholder, moet in _Layout komen

---
+++ Hoekstra Fotografie/Hoekstra Fotografie/Website Hoekstra/Website Hoekstra/Pages/Login.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website_Hoekstra.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty] public ValuePhoto NewPhoto { get; set; } = new ValuePhoto();
+        [BindProperty] public user_controller NewUser { get; set; } = new user_controller();
+        
+        public List<user_controller> Users
+        {
+            get
+            {
+                return new DBRepos().GetUsers();
+            }
+        }
+
+        public void OnGet()
+        {
+
+        }
+        
+
+        public void OnPost()
+        {
+            
+        }
+
+        public void OnPostTryAddUser()
+        {
+            if (checkUsernameAvailable())
+            {
+                new DBRepos().tryAddUser(NewUser);
+            }
+            else
+            {
+                
+            }
+        }
+
+        public bool checkUsernameAvailable()
+        {
+
+            foreach (var user in Users)
+            {
+                if (NewUser.username.Equals(user.username))
+                {
+                    Console.WriteLine("gebruiker bestaat");
+                    return false;
+                }
+            }
+            Console.WriteLine("gebruiker bestaat NIET");
+            return true;
+        }
+    }
+}
