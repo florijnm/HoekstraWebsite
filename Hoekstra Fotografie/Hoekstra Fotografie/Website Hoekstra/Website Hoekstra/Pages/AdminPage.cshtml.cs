ddc50186c0f07482e9f5525f1b15669bce2289a9
Florijnm <florijn.munster@student.nhlstenden.com>
2021-03-29 14:33:03 +0200

    test

--- Hoekstra Fotografie/Hoekstra Fotografie/Website Hoekstra/Website Hoekstra/Pages/AdminPage.cshtml.cs
+++ Hoekstra Fotografie/Hoekstra Fotografie/Website Hoekstra/Website Hoekstra/Pages/AdminPage.cshtml.cs
@@ -10,10 +10,27 @@ namespace Website_Hoekstra.Pages
     public class AdminPageModel : PageModel
     {
         [BindProperty] public ValuePhoto NewPhoto { get; set; } = new ValuePhoto();
-
+        [BindProperty] public string Label { get; set; }
         public void OnGet()
         {
+            string cookieStr = Request.Cookies["cookieLogin"];
+            if (cookieStr != null)
+            {
+                Label = "Welcome, " + FirstLetterToUpper(cookieStr.ToString()) + "!";
+            }
+        }
 
+        public string FirstLetterToUpper(string str)
+        {
+            if (str == null)
+            {
+                return null;
+            }
+            if (str.Length > 1) 
+            {
+                return char.ToUpper(str[0]) + str.Substring(1);
+            }
+            return str.ToUpper();
         }
 
         public List<category_ids> category
---
+++ Hoekstra Fotografie/Hoekstra Fotografie/Website Hoekstra/Website Hoekstra/Pages/Login.cshtml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website_Hoekstra.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty] public user_controller User { get; set; } = new user_controller();
+
+        [BindProperty] public login_user LoginUser { get; set; } = new login_user();
+        [BindProperty] public string Label { get; set; }
+
+        public List<user_controller> Users
+        {
+            get
+            {
+                return new DBRepos().GetUsers();
+            }
+        }
+
+        public void OnGet()
+        {
+            Label = "false";
+            
+        }
+        
+
+        public void OnPost()
+        {
+            
+        }
+
+        public void OnPostTryAddUser()
+        {
+            if (CheckUsernameAvailable())
+            {
+                new DBRepos().tryAddUser(User);
+            }
+            else
+            {
+                
+            }
+        }
+
+        public void OnPostVerifyUser()
+        {
+            if (VerifyPassword())
+            {
+                // login succesvol
+                Label = "true";
+                Response.Cookies.Append("cookieLogin", LoginUser.loginUsername);
+                if (User.admin == true)
+                {
+                    Response.Redirect("adminpage");
+                }
+                else
+                {
+                    Response.Redirect("Error");
+                }
+            }
+            else
+            {
+                Label = "false";
+            }
+        }
+        public bool CheckUsernameAvailable()
+        {
+
+            foreach (var user in Users)
+            {
+                if (User.username.Equals(user.username))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool VerifyPassword()
+        {
+            if (LoginUser.loginUsername != null && LoginUser.loginUsername != null )
+            {
+                foreach (var dbUser in Users)
+                {
+                    if (String.Compare(LoginUser.loginUsername,dbUser.username) == 0)
+                    {
+                        if (new DBRepos().verifyPass(LoginUser, dbUser.password))
+                        {
+                            User.admin = dbUser.admin;
+                            User.username = dbUser.username;
+                            User.password = dbUser.password;
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+
+                return false;
+            }
+            return false;
+        }
+    }
+}
